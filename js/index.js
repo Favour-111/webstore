2d14535a8e40bc94082e35a1c54cfbb5b695c178
favour <omojolaobaloluwa@gmail.com>
2024-05-08 12:30:24 +0100

    done

--- js/index.js
+++ js/index.js
@@ -1,31 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const carouselInner = document.querySelector(".carousel-inner");
-  const carouselItems = document.querySelectorAll(".carousel-item");
-  const totalItems = carouselItems.length;
-  const itemWidth = carouselItems[0].offsetWidth;
-  let currentIndex = 0;
-
-  function nextSlide() {
-    currentIndex = (currentIndex + 1) % totalItems;
-    updateCarousel();
-  }
-
-  function prevSlide() {
-    currentIndex = (currentIndex - 1 + totalItems) % totalItems;
-    updateCarousel();
-  }
-
-  function updateCarousel() {
-    carouselInner.style.transform = `translateX(-${
-      currentIndex * itemWidth
-    }px)`;
-  }
-
-  setInterval(nextSlide, 2000); // Change slide every 2 seconds
-
-  const prevBtn = document.querySelector(".prev-btn");
-  const nextBtn = document.querySelector(".next-btn");
-
-  prevBtn.addEventListener("click", prevSlide);
-  nextBtn.addEventListener("click", nextSlide);
-});
+function navigateTo(section) {
+  window.location.href = section + ".html";
+}
